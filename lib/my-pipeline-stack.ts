f5a594521fb51743eb5b0e4a127f21fabe6a34b7
EC2 Default User <ec2-user@ip-172-31-35-61.ap-southeast-2.compute.internal>
2023-04-19 03:50:40 +0000

    initial commit 3

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -2,20 +2,26 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { SecretValue } from "aws-cdk-lib";
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    const token = SecretValue.secretsManager("github-token")
+    console.log('helllppppppppppp')
+    console.log(token)
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyPipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('dianibar/my-pipeline', 'main',
         {
-				  authentication: SecretValue.secretsManager("github-token")
+				  authentication: token
 			  }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
+     pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: { account: "145427425754", region: "ap-southeast-2" }
+    }));
   }
 }
